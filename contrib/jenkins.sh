c35bb473e449ec8b0d6e6ab64196331e89efef95
Max <msuraev@sysmocom.de>
2017-12-15 11:56:05 +0100

    Expand CI tests
    * make sure jenkins.sh fails on any errors similar to other jenkins jobs
    * always explicitly use python2 instead of generic python
    * add basic module import tests for python 2 and 3
    * add comments
    
    Change-Id: I0f4639537d227c513859d4552533ce1e41df9deb

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-python2 ./setup.py install || python ./setup.py install
+
+set -ex
+
+# FIXME: remove once python 2 support is deprecated
+python2 ./setup.py install
+python2 tests/test_py2.py
+
 rm -rf ./build
 python3 ./setup.py install
+python3 tests/test_py3.py
+
+# TODO: add more tests
---
+++ tests/test_py3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# just import a smoke test for osmopy
+
+import osmopy
+
+print('[Python3] Smoke test PASSED.')
