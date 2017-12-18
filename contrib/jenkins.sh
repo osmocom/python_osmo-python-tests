1a5364696eaf66a382e6d451bc4255622d845a92
Max <msuraev@sysmocom.de>
2017-12-18 10:58:12 +0100

    Explicitly check syntax in jenkins tests
    By default setup.py ignores compilation errorsand proceed with
    installation anyway. Make sure that it fails if any errorsis encountered
    bymanually call compilation step.
    
    Change-Id: I2235e3fff21b7c09d3fba00ecdccca7f4515e071
    Related: OS#2770

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -5,9 +5,11 @@ set -ex
 # FIXME: remove once python 2 support is deprecated
 python2 ./setup.py install
 python2 tests/test_py2.py
+python2 -m compileall osmopy
 
 rm -rf ./build
 python3 ./setup.py install
 python3 tests/test_py3.py
+python3 -m compileall osmopy
 
 # TODO: add more tests
