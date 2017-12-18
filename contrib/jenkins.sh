6588960d1d27d5a6da7fb6144e72f6f134e69063
Max <msuraev@sysmocom.de>
2017-12-18 15:05:41 +0100

    Add scripts to compilation tests in jenkins
    Related: OS#2684
    Change-Id: If098c20b1ad17c3868073b9fe07fd3795217c526

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -2,14 +2,26 @@
 
 set -ex
 
+COM_FLAGS='-m compileall'
+
 # FIXME: remove once python 2 support is deprecated
-python2 ./setup.py install
-python2 tests/test_py2.py
-python2 -m compileall osmopy
+PY2=python2
+PY2_LIST="osmopy scripts/osmo_ctrl.py scripts/osmodumpdoc.py scripts/osmotestvty.py scripts/osmotestconfig.py"
+$PY2 ./setup.py install
+$PY2 tests/test_py2.py
+for f in $PY2_LIST
+do
+    $PY2 $COM_FLAGS $f
+done
 
 rm -rf ./build
-python3 ./setup.py install
-python3 tests/test_py3.py
-python3 -m compileall osmopy
+PY3=python3
+PY3_LIST="osmopy scripts/osmo_ctrl.py scripts/osmo_interact_ctrl.py scripts/osmo_interact_vty.py scripts/osmo_verify_transcript_ctrl.py scripts/osmo_verify_transcript_vty.py scripts/soap.py scripts/twisted_ipa.py"
+$PY3 ./setup.py install
+$PY3 tests/test_py3.py
+for f in $PY3_LIST
+do
+    $PY3 $COM_FLAGS $f
+done
 
 # TODO: add more tests
